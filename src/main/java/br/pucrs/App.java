37d5a017efff8ca1b3af68709cf0c5bfda7c58b6
João Pedro da Silva Kriger <128426379+sillysecret@users.noreply.github.com>
2025-03-13 01:09:12 +0000

    MergeSort

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -1,13 +1,64 @@
 package br.pucrs;
 
-/**
- * Hello world!
- *
- */
+import java.lang.reflect.Array;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int[] arr = { 5, 2, 9, 1, 5, 6 };
+        arr = MergeSort(arr);  
+        for (int num : arr) {
+            System.out.print(num + " "); 
+        }
+    }
+
+    public static int[] MergeSort(int[] arr){
+        if(arr.length == 1)
+            return arr; 
+        
+            int n = arr.length;
+ 
+            int[] a = new int[(n + 1)/2];
+            int[] b = new int[n - a.length];
+     
+            for (int i = 0; i < n; i++)
+            {
+                if (i < a.length) {
+                    a[i] = arr[i];
+                }
+                else {
+                    b[i - a.length] = arr[i];
+                }
+            }
+            
+            a = MergeSort(a);
+            b = MergeSort(b);
+            int[] l = Merge(a,b);
+            
+            return l;
+    }
+
+    private static int[] Merge(int[] a, int[] b) {
+        int[] merged = new int[a.length + b.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                merged[k++] = a[i++];
+            } else {
+                merged[k++] = b[j++];
+            }
+        }
+
+        while (i < a.length) {
+            merged[k++] = a[i++];
+        }
+
+        while (j < b.length) {
+            merged[k++] = b[j++];
+        }
+
+        return merged;
     }
 }
