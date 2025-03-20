d29e8d60c1c29386e796dc2da15d7fb0ff81da84
João Miguel <j.meier@edu.pucrs.br>
2025-03-20 01:08:46 +0000

    adding exercises from the latest class

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -13,13 +13,20 @@ public class App
 
         long startTime = System.nanoTime();
         //arr = MergeSort(arr);
-        long maxValue = maxVal2(arr, 0, 1048575);  
+        //long maxValue = maxVal2(arr, 0, 1048575);  
+
+        //long multiplier4bit = multiply(15, 10, 4);
+        //long multiplier16bit = multiply(32767, 16384, 16);
+        long multiplier64bit = multiply(9223372036854775807L, 123456789L, 64);
+
 
         long endTime = System.nanoTime();
         long duration = endTime - startTime;
         
         System.out.println("Tempo de execução: " + duration + " nanosegundos.");
-        System.out.println("MaxValue: " + maxValue);
+        //System.out.println("Multiplier 4 bits: " + multiplier4bit);
+        //System.out.println("Multiplier 16 bits: " + multiplier16bit);
+        System.out.println("Multiplier 64 bits: " + multiplier64bit);
         System.out.println("Cont: " + cont);
     }
 
@@ -95,7 +102,7 @@ public class App
         return array;
     }
 
-    static long maxVal2(long A[], int init, int end) { 
+    private static long maxVal2(long A[], int init, int end) { 
         cont++;
         if (end - init <= 1)
             return Math.max(A[init], A[end]);  
@@ -105,5 +112,52 @@ public class App
               long v2 = maxVal2(A,m+1,end);  
               return Math.max(v1,v2);
              }
+    }
+
+    private static long multiply(long x, long y, long n){
+        cont++;
+        if (n == 1) return x * y;
+        else{
+            int m = (int) Math.ceil(n / 2.0);
+            long a = x / (long) (Math.pow(2,m));
+            long b = x % (long) (Math.pow(2,m));
+            long c = y / (long) (Math.pow(2,m));
+            long d = y % (long) (Math.pow(2,m));
+
+            long e = multiply(a, c, m);
+            long f = multiply(b, d, m);
+            long g = multiply(b, c, m);
+            long h = multiply(a, d, m);
+
+            return (long) (Math.pow(2, 2*m) * e) + (long) (Math.pow(2,m) * (g + h) + f);
+        }
+    }
+
+    private static long multiplyString(String X, String Y) {
+        int n = Math.max(X.length(), Y.length());
+        
+        return multiplyRecursive(X, Y, n);
+    }
+
+    private static long multiplyRecursive(String X, String Y, int n) {
+        cont++;  
+
+        if (n == 1) {
+            return X.charAt(0) == '1' && Y.charAt(0) == '1' ? 1 : 0;
+        } else {
+            int m = (int) Math.ceil(n / 2.0);
+
+            String X1 = X.substring(0, m); 
+            String X2 = X.substring(m);    
+            String Y1 = Y.substring(0, m); 
+            String Y2 = Y.substring(m);    
+
+            long e = multiplyRecursive(X1, Y1, m);
+            long f = multiplyRecursive(X2, Y2, m);
+            long g = multiplyRecursive(X2, Y1, m);
+            long h = multiplyRecursive(X1, Y2, m);
+
+            return (long) (Math.pow(2, 2 * m) * e + Math.pow(2, m) * (g + h) + f);
+        }
     }
 }
